7c96daad2f9f6bfa26143be28b77419a3af63ce9
Nov1c444 <857526207@qq.com>
2023-08-12 21:53:10 +0800

    feat: init the app

---
+++ recipes_app/recipes_app/recipes_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipes_appApp.swift
+//  recipes_app
+//
+//  Created by Novice on 2023/8/12.
+//
+
+import SwiftUI
+
+@main
+struct recipes_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
